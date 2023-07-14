aa208ce56ed30bcc5a11bf7de02032a717a94bdb
Sudha <Sudha.MadhuriPoojari@appliedtechnology.se>
2023-07-14 10:13:15 +0200

    created frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Login from './Components/Login'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Login />
+      </div>
+    </>
+  )
+}
+
+export default App
