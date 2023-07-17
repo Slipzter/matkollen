555483b34e91d95df905b267f3c1ac79753d8682
ica1130 <ilija.krilovic@appliedtechnology.se>
2023-07-17 15:12:09 +0200

    Search page sends a GET request. Need to update to which endpoint.

--- frontend/src/app/api/search.ts
+++ frontend/src/app/api/search.ts
@@ -4,8 +4,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const { q } = req.query;
-
-
-    res.status(200).json({message: "Hej!"});
+    if (req.method === "GET") {
+        try {
+            const { q } = req.query;
+            res.status(200).json({message: "Hej!"});
+        } catch (error) {}
+    }
+    
 }
--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -1,11 +1,27 @@
 'use client'
 
 import { useSearchParams } from "next/navigation";
+import useSWR from 'swr';
+
+const fetchProducts = async (url: string) => {
+    const response = await fetch(url)
+
+    if(!response.ok) {
+        throw new Error('Failed to fetch products');
+    }
+
+    return response.json();
+}
 
 const SearchPage = () => {
     const search = useSearchParams();
     const searchQuery = search ? search.get('q') : null;
     const encodedSearchQuery = encodeURI(searchQuery || "");
+
+    const { data, isLoading } = useSWR(
+        `/api/search?q=${encodedSearchQuery}`,
+        fetchProducts
+    );
 
 
     console.log('SEARCH PARAMS', encodedSearchQuery);
