10a440068065ff98a23d932b6ea87f18af0cbf9d
Tim Hansson Meng <timhanssonmeng@gmail.com>
2023-07-18 15:07:18 +0200

    fix: energy card

--- frontend/src/app/productInfo/page.tsx
+++ frontend/src/app/productInfo/page.tsx
@@ -15,7 +15,7 @@ function ProductInfoPage() {
       <img className="product-info__image" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80" alt="" />
       <h1 className="product-info__title">{productName}</h1>
       <section className="product-info__card-section">
-        <article className="product-info__energy-card">
+        <article className="card product-info__energy-card">
           <h3>Energy</h3>
           <h2>{calories} Kcal</h2>
         </article>
