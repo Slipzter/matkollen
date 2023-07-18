be2c7ca7ed29b15818a45c4723c5a632835bb55a
ica1130 <ilija.krilovic@appliedtechnology.se>
2023-07-18 15:41:40 +0200

    added style for home page and search bar.

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -4,12 +4,7 @@ import Link from "next/link";
 export default function Home() {
     return (
     <>
-    <nav className="sticy top-0 px-2 py-4">
-        <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
-            <Link href="/landingpage">Back</Link>
-        </ul>
-    </nav>
-    <main>
+    <main className="flex justify-center align-middle">
         <SearchInput />
     </main>
     </>
