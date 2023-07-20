ef111eebd4fc505ec25fcec46cc0cf58d0799616
Tim Hansson Meng <timhanssonmeng@gmail.com>
2023-07-20 16:41:04 +0200

    added more styling

--- frontend/src/app/(home)/home/layout.tsx
+++ frontend/src/app/(home)/home/layout.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import '../globals.css';
-import type { Metadata } from 'next';
+import '../../globals.css';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -25,7 +24,6 @@ export default function RootLayout({
           <button className="nav-back nav-button" onClick={handleGoBack}><i className="fa fa-arrow-circle-left"></i></button>
           <nav>
             <ul>
-              <Link className="nav-home nav-button" href="/home"><i className="fa fa-search"></i></Link>
               <Link className="nav-landing nav-button" href="/landingpage"><i className="fa fa-user-circle"></i></Link>
             </ul>
           </nav>
--- frontend/src/app/landingpage/page.tsx
+++ frontend/src/app/landingpage/page.tsx
@@ -11,17 +11,20 @@ const landing = () => {
   return (
     <>
       <div className='landing'>
+      <div className='landing-background'>
       <img className= 'logo-img' src="/bluetea-logo.png" alt="" />
         <div className='logo'>
         {/* <img className= 'logo-img' src="/logo.png" alt="" /> */}
-        <h1>MATKOLLEN</h1>
+        <h1 className='app-title'>MATKOLLEN</h1>
         </div>
+
       <div className='footer'>
         <button className='login' onClick={handleSearch}>
           Logga in
         </button>
         <Link href={`/home`} className='continue-link'>Fortsätt som gäst</Link>
       </div>
+        </div>
       </div>
     </>
   )
