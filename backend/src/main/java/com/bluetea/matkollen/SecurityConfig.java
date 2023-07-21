9851f1de901e1fda0e95becc9bcf385874884c64
Tim Hansson Meng <timhanssonmeng@gmail.com>
2023-07-21 13:25:34 +0200

    feat: integrated google authentication

--- backend/src/main/java/com/bluetea/matkollen/SecurityConfig.java
+++ backend/src/main/java/com/bluetea/matkollen/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.bluetea.matkollen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,13 +14,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    @Autowired
+    CustomSuccessHandler handler;
+
+/*    @Bean
     DefaultSecurityFilterChain defaultChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth ->
                         auth.requestMatchers("/**").permitAll()
                 )
                 .cors(withDefaults())
             .build();
+    }*/
+
+    @Bean
+    DefaultSecurityFilterChain defaultChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth ->
+                auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/home").authenticated()
+        )          .oauth2Login(login -> login.successHandler(handler))
+                .cors(withDefaults())
+//                .oauth2ResourceServer(it -> it.jwt(withDefaults()))
+                .csrf().disable()
+                .build();
     }
 }
 
--- backend/src/test/java/com/bluetea/matkollen/ApplicationTestConfig.java
+++ backend/src/test/java/com/bluetea/matkollen/ApplicationTestConfig.java
@@ -2,7 +2,6 @@ package com.bluetea.matkollen;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
