f9bc41e0ef50548ae498b6eb6b3fc10c61d6ec33
Sudha <Sudha.MadhuriPoojari@appliedtechnology.se>
2023-07-24 11:49:41 +0200

    add select drop down and button

--- frontend/src/app/(pages)/userProfile/page.tsx
+++ frontend/src/app/(pages)/userProfile/page.tsx
@@ -1,22 +1,55 @@
 'use client'
 import SearchInput from "@/app/component/SearchInput";
-import Card from "@/app/component/Card";
+import { useEffect, useState } from "react";
 
 function UserProfile(){
+    const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+    const [currentOption, setCurrentOption] = useState<string>("");
 
+    useEffect(()=> {
+        const storedOptions = JSON.parse(localStorage.getItem("selectedOptions"));
+        if(storedOptions){
+            setSelectedOptions(storedOptions)
+        }
 
+    }, []);
 
+    useEffect(()=>{
+       localStorage.setItem("selectedOptions", JSON.stringify(selectedOptions))
+    }, [selectedOptions]);
+
+    const handleOptionChange = (e: any) =>{
+        setCurrentOption(e.target.value)
+        console.log("Added",e.target.value )
+    }
+
+    const handleAddOption = ()=>{
+        if (currentOption && !selectedOptions.includes(currentOption)) {
+            setSelectedOptions([...selectedOptions, currentOption]);
+            setCurrentOption("");
+          }
+    };
 
     return(
         <>
         <div className="user-container">
         <SearchInput />
-         <h4>Welcome to Matkollen {} !</h4>
+         <h4>Välkommen till Matkollen {} !</h4>
         </div> 
-        <div>
-            <Card energi_kcal={0} fett_totalt_g={0} kolhydrater_g={0} livsmedelsnamn={""} protein_g={0} searchQuery={""} />
+        <div className="select-options">
+        <select name="preferences" className="options" value={currentOption} onChange={handleOptionChange} >
+            <option value="">Välj</option>
+            <option value="fiber">Fiber</option>
+            <option value="vatten">Vatten</option>
+            <option value="alcohol">Alcohol</option>
+            <option value="aska">Aska</option>
+            <option value="sockerarter">Sockerarter</option>
+            <option value="kalcium">Kalcium</option>
+            <option value="iron">Iron</option>
+        </select>
+        <button className="add" id = "addButton" onClick = {handleAddOption}>Lägg till</button>
         </div>
-        </>
+    </>
     )
 }
 
