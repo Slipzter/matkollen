040eaf1002d893e6ef5d2a62fa5dab285930eb0a
Sudha <Sudha.MadhuriPoojari@appliedtechnology.se>
2023-07-24 10:42:02 +0200

    search button styled and created user profile page

--- frontend/src/app/(pages)/userProfile/page.tsx
+++ frontend/src/app/(pages)/userProfile/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import SearchInput from "@/app/component/SearchInput";
+import Card from "@/app/component/Card";
 
 function UserProfile(){
 
@@ -7,11 +9,13 @@ function UserProfile(){
 
     return(
         <>
-        <div className="search-page">
-         User Profile
-
+        <div className="user-container">
+        <SearchInput />
+         <h4>Welcome to Matkollen {} !</h4>
         </div> 
-
+        <div>
+            <Card energi_kcal={0} fett_totalt_g={0} kolhydrater_g={0} livsmedelsnamn={""} protein_g={0} searchQuery={""} />
+        </div>
         </>
     )
 }
