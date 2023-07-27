5c755c596dcd7266b34c225620c5bc3fac16ffc0
ica1130 <ilija.krilovic@appliedtechnology.se>
2023-07-27 11:43:44 +0200

    name added to homepage.

--- frontend/src/app/landingpage/page.tsx
+++ frontend/src/app/landingpage/page.tsx
@@ -14,7 +14,7 @@ const landing = () => {
         </div>
 
       <div className='footer'>
-        <Link className='login' href='https://blue-tea-matkollen-app.azurewebsites.net/login'>
+        <Link className='login' href='http://localhost:8080/login'>
           Logga in
         </Link>
         <Link href={`/guest/home`} className='continue-link'>Fortsätt som gäst</Link>
--- frontend/src/app/user/(pages)/home/page.tsx
+++ frontend/src/app/user/(pages)/home/page.tsx
@@ -1,8 +1,8 @@
+'use client'
 
 import UserSearchInput from "@/app/(components)/UserSearchInput";
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-
 
 // const getName = async () => {
 //   const searchParams = useSearchParams();
@@ -14,14 +14,18 @@ import { useSearchParams } from 'next/navigation';
 
 async function Home() {
 
-    // const name = await getName();
+  const searchParams = useSearchParams();
+  const id = searchParams ? searchParams.get('id') : null;
+  const response = await fetch(process.env.NEXT_PUBLIC_DOMAIN + '/user/home/' + id);
+  const json = await response.json();
+  const name =  json.name;
 
     return (
     <>
     <main className="home">
       <div className="home__image"></div>
 
-      <h2>Welcome!</h2>
+      <h2>Welcome {name}</h2>
       <Link href="/user/userProfile" className="prefences"> Set your prefences </Link> 
 
         <UserSearchInput />
